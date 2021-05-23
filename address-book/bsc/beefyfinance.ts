ec23696a962f89accdb6021cf93d9c1e49248da1
Moonster <Moonster.bsc@gmail.com>
2021-05-23 16:58:59 -0400

    pancake

--- address-book/bsc/beefyfinance.ts
+++ address-book/bsc/beefyfinance.ts
@@ -1,5 +1,5 @@
 const beefyfinance = {
-  cowllector: "",
+  multisig: "0x8f0fFc8C7FC3157697Bdbf94B328F7141d6B41de",
   keeper: "0xd529b1894491a0A26b18939274aE8EDe93E81DbA",
   beefyFeeRecipient: "0xeb41298ba4eA3865C33BDE8f60EC414421050d53",
 };
--- address-book/bsc/pancake.ts
+++ address-book/bsc/pancake.ts
@@ -1,7 +1,7 @@
 const pancake = {
-  masterchef: "",
-  router: "",
-  oldRouter: "", // For historical reasons only, shouldn't use this for anything new
+  masterchef: "0x73feaa1ee314f8c655e354234017be2193c9e24e",
+  router: "0x10ED43C718714eb63d5aA57B78B54704E256024E",
+  oldRouter: "0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F", // For historical reasons only, shouldn't use this for anything new
 };
 
 export default pancake;

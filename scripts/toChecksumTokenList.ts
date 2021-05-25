e8e545ebc8517f6968cfc908915af73691e5a1ff
Moonster <Moonster.bsc@gmail.com>
2021-05-25 17:17:31 -0400

    token list map

--- scripts/toChecksumTokenList.ts
+++ scripts/toChecksumTokenList.ts
@@ -5,6 +5,13 @@ import fetch from "node-fetch";
 import transformTokenListToObject from "../util/transfomTokenListToObject";
 import chainIdMap from "../util/chainIdMap";
 
+const tokenLists = {
+  pancake:
+    "https://gateway.pinata.cloud/ipfs/QmdKy1K5TMzSHncLzUXUJdvKi1tHRmJocDRfmCXxW5mshS",
+  quickswap:
+    "https://unpkg.com/quickswap-default-token-list@1.0.59/build/quickswap-default.tokenlist.json",
+};
+
 const toChecksumTokenList = (tokenList: TokenList, chainId?: number): void => {
   const tokens = tokenList.tokens;
   for (const token of tokens) {
@@ -13,10 +20,8 @@ const toChecksumTokenList = (tokenList: TokenList, chainId?: number): void => {
 };
 
 (async () => {
-  const chainId = chainIdMap.bsc;
-  const response = await fetch(
-    "https://gateway.pinata.cloud/ipfs/QmdKy1K5TMzSHncLzUXUJdvKi1tHRmJocDRfmCXxW5mshS"
-  );
+  const chainId = chainIdMap.polygon;
+  const response = await fetch(tokenLists.quickswap);
   const tokenList = (await response.json()) as unknown as TokenList;
   toChecksumTokenList(tokenList);
   const toMap = transformTokenListToObject(tokenList, chainId);

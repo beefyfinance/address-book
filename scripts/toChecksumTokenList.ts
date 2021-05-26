a8bb0ae97497d79859def3460f5d966cd1da70e5
Moonster <Moonster.bsc@gmail.com>
2021-05-25 20:57:19 -0400

    didn't make a diff

--- scripts/toChecksumTokenList.ts
+++ scripts/toChecksumTokenList.ts
@@ -9,21 +9,6 @@ import chainIdMap from "../util/chainIdMap";
 const tokenLists = {
   bsc: [
     "https://gateway.pinata.cloud/ipfs/QmdKy1K5TMzSHncLzUXUJdvKi1tHRmJocDRfmCXxW5mshS",
-    "https://yearn.science/static/tokenlist.json",
-    "https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json",
-    "tokenlist.aave.eth",
-    "synths.snx.eth",
-    "https://umaproject.org/uma.tokenlist.json",
-    "https://nftx.ethereumdb.com/v1/tokenlist/",
-    "wrapped.tokensoft.eth",
-    "https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json",
-    "https://raw.githubusercontent.com/opynfinance/opyn-tokenlist/master/opyn-v1.tokenlist.json",
-    "https://app.tryroll.com/tokens.json",
-    "defi.cmc.eth",
-    "stablecoin.cmc.eth",
-    "t2crtokens.eth",
-    "https://www.gemini.com/uniswap/manifest.json",
-    "https://unpkg.com/quickswap-default-token-list@1.0.39/build/quickswap-default.tokenlist.json",
   ],
   quickswap: [
     "https://unpkg.com/quickswap-default-token-list@1.0.59/build/quickswap-default.tokenlist.json",

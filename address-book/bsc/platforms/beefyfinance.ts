3a42c7915eaeb45cb71d42c0d37e70ae72fe6a49
Moonster <Moonster.bsc@gmail.com>
2021-05-25 20:00:38 -0400

    as const bsc

--- address-book/bsc/platforms/beefyfinance.ts
+++ address-book/bsc/platforms/beefyfinance.ts
@@ -2,4 +2,4 @@ export const beefyfinance = {
   multisig: "0x8f0fFc8C7FC3157697Bdbf94B328F7141d6B41de",
   keeper: "0xd529b1894491a0a26B18939274ae8ede93E81dbA",
   beefyFeeRecipient: "0xEB41298BA4Ea3865c33bDE8f60eC414421050d53",
-};
+} as const;

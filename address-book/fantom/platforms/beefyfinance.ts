24c02adce38fa1a955115b8ce8f105763e74e994
sirbeefalot <sirbeefalot@pm.me>
2021-06-01 15:43:10 -0300

    Update fantom fee batcher

--- address-book/fantom/platforms/beefyfinance.ts
+++ address-book/fantom/platforms/beefyfinance.ts
@@ -5,5 +5,5 @@ export const beefyfinance = {
   keeper: cowllector,
   rewardPool: "0x7fB900C14c9889A559C777D016a885995cE759Ee",
   treasury: "0xe6CcE165Aa3e52B2cC55F17b1dBC6A8fe5D66610",
-  beefyFeeRecipient: "0x0b1d21660EC88a4e56B79420AA15627835b09a04",
+  beefyFeeRecipient: "0x4f8865A1FcE2877cCB55264600D4759d222E8fEB",
 } as const;

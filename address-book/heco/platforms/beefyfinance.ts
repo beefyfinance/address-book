ce7ade5e79ad8f6033ed7684b340225c75101919
sirbeefalot <sirbeefalot@pm.me>
2021-06-02 13:24:55 -0300

    Add new beefy admin accounts for each chain.

--- address-book/heco/platforms/beefyfinance.ts
+++ address-book/heco/platforms/beefyfinance.ts
@@ -1,8 +1,9 @@
-const cowllector = "0xd529b1894491a0a26B18939274ae8ede93E81dbA";
-
 export const beefyfinance = {
-  cowllector,
-  keeper: cowllector,
+  strategyOwner: "0x6d28afD25a1FBC5409B1BeFFf6AEfEEe2902D89F",
+  vaultOwner: "0x4E2a43a0Bf6480ee8359b7eAE244A9fBe9862Cdf",
+  keeper: "0x10aee6B5594942433e7Fc2783598c979B030eF3D",
+  rewarder: "0xd529b1894491a0a26B18939274ae8ede93E81dbA",
+  treasurer: "0x565EB5e5B21F97AE9200D121e77d2760FFf106cb",
   rewardPool: "0x5f7347fedfD0b374e8CE8ed19Fc839F59FB59a3B",
   treasury: "0xf4859A3f36fBcA24BF8299bf56359fB441b03034",
   beefyFeeRecipient: "0x183D1aaEf1a86De6f16B2737c30eF94a6d2A9308",
--- address-book/polygon/platforms/beefyfinance.ts
+++ address-book/polygon/platforms/beefyfinance.ts
@@ -1,8 +1,9 @@
-const cowllector = "0xd529b1894491a0a26B18939274ae8ede93E81dbA";
-
 export const beefyfinance = {
-  cowllector,
-  keeper: cowllector,
+  strategyOwner: "0x6d28afD25a1FBC5409B1BeFFf6AEfEEe2902D89F",
+  vaultOwner: "0x4E2a43a0Bf6480ee8359b7eAE244A9fBe9862Cdf",
+  keeper: "0x10aee6B5594942433e7Fc2783598c979B030eF3D",
+  rewarder: "0xd529b1894491a0a26B18939274ae8ede93E81dbA",
+  treasurer: "0x565EB5e5B21F97AE9200D121e77d2760FFf106cb",
   rewardPool: "0xDeB0a777ba6f59C78c654B8c92F80238c8002DD2",
   treasury: "0x09EF0e7b555599A9F810789FfF68Db8DBF4c51a0",
   beefyFeeRecipient: "0xA323f6761c6eF06B6EAB06D6138B15486b0e96da",

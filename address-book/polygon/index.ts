001221fb009ec65b3a4a2096b6d4a76d172163a5
Moonster-BSC <83176690+Moonster-BSC@users.noreply.github.com>
2021-06-07 10:56:21 -0400

    gbull (#12)

--- address-book/polygon/index.ts
+++ address-book/polygon/index.ts
@@ -1,9 +1,10 @@
 import { beefyfinance } from "./platforms/beefyfinance";
 import { cometh } from "./platforms/cometh";
-import { dyfn } from "./platforms/dyfn";
+import { dfyn } from "./platforms/dfyn";
 import { polyzap } from "./platforms/polyzap";
 import { quickswap } from "./platforms/quickswap";
 import { sushi } from "./platforms/sushi";
+import { goldenbull } from "./platforms/goldenbull";
 import { tokens } from "./tokens/tokens";
 import { convertSymbolTokenMapToAddressTokenMap } from "../../util/convertSymbolTokenMapToAddressTokenMap";
 
@@ -11,10 +12,11 @@ export const polygon = {
   platforms: {
     beefyfinance,
     cometh,
-    dyfn,
+    dfyn,
     polyzap,
     quickswap,
     sushi,
+    goldenbull,
   },
   tokens,
   tokenAddressMap: convertSymbolTokenMapToAddressTokenMap(tokens),
